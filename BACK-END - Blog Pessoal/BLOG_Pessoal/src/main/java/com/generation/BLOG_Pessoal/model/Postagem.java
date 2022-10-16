5ec6883d210a15496a42513a45e32ba5dd5fdd1b
thaissevero <thais.severo21@gmail.com>
2022-10-16 20:36:07 -0300

    Atividade do dia 13/10 e 14/10

--- BACK-END - Blog Pessoal/BLOG_Pessoal/src/main/java/com/generation/BLOG_Pessoal/model/Postagem.java
+++ BACK-END - Blog Pessoal/BLOG_Pessoal/src/main/java/com/generation/BLOG_Pessoal/model/Postagem.java
@@ -37,6 +37,10 @@ public class Postagem {
     @ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
+    
+    @ManyToOne
+    @JsonIgnoreProperties("postagem")
+    private Usuario usuario;
 
     /*Insira os Getters and Setters*/
 
@@ -80,4 +84,11 @@ public class Postagem {
 		this.tema = tema;
 	}
 
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 }
