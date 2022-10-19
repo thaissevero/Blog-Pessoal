3dc6efa643e0b3ecf2a99928f501f986fa8365be
thaissevero <thais.severo21@gmail.com>
2022-10-19 17:33:48 -0300

    Deploy do Projeto BLOG Pessoal

--- BACK-END - Blog Pessoal/BLOG_Pessoal/src/main/java/com/generation/BLOG_Pessoal/model/Usuario.java
+++ BACK-END - Blog Pessoal/BLOG_Pessoal/src/main/java/com/generation/BLOG_Pessoal/model/Usuario.java
@@ -14,6 +14,8 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name = "tb_usuarios")
 public class Usuario {
@@ -29,6 +31,7 @@ public class Usuario {
 			message = "O link da foto não pode ser maior do que 5000 caracteres")
 			private String foto;
 	
+	@Schema(example = "email@email.com.br")
 	@NotBlank (message = "O atributo Usuário é Obrigatório!")
 	@Email(message = "O atributo Usuário deve ser um email válido!")
 	private String usuario;
